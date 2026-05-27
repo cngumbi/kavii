00301fd67497def3d0fa25c4b93fb3702d15cd02
cngumbi <cngumbi35@gmail.com>
2026-05-27 22:58:22 +0300

    css library

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function build() {
-    return src('index.scss')
-        .pipe(sass())
+    return src('ncw/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(dest('css'))
 }
 
 function liveTask() {
-    watch(['index.scss'], build)
+    watch(['ncw/**/*.scss'], build)
 }
 
 exports.default = series(build, liveTask)
