b7942ac2a39c777f97e8264871f7f0cbe4089c7f
cngumbi <cngumbi35@gmail.com>
2026-05-29 13:47:48 +0300

    css library

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,22 @@
+//for production
+//const { src, dest, watch, series } = require('gulp');
+//const sass = require('gulp-sass')(require('sass'));
+//const purgecss = require('gulp-purgecss');
+//
+//function build() {
+//    return src('ncw/**/*.scss')
+//        .pipe(sass().on('error', sass.logError))
+//        .pipe(purgecss({ content: ['*.html']}))
+//        .pipe(dest('css'))
+//}
+//
+//function liveTask() {
+//    watch(['ncw/**/*.scss', '*.html'], build)
+//}
+//
+//exports.default = series(build, liveTask)
+
+//development
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
