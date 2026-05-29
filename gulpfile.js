a6d8d02ebf4d6c28de43b11b7a6264339d835ec1
cngumbi <cngumbi35@gmail.com>
2026-05-29 17:44:00 +0300

    css library

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 //for production
-//const { src, dest, watch, series } = require('gulp');
-//const sass = require('gulp-sass')(require('sass'));
-//const purgecss = require('gulp-purgecss');
-//
-//function build() {
-//    return src('ncw/**/*.scss')
-//        .pipe(sass().on('error', sass.logError))
-//        .pipe(purgecss({ content: ['*.html']}))
-//        .pipe(dest('css'))
-//}
-//
-//function liveTask() {
-//    watch(['ncw/**/*.scss', '*.html'], build)
-//}
-//
-//exports.default = series(build, liveTask)
-
-//development
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function build() {
     return src('ncw/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('/css'))
+        .pipe(purgecss({ content: ['*.html']}))
+        .pipe(dest('css'))
 }
 
 function liveTask() {
-    watch(['ncw/**/*.scss'], build)
+    watch(['ncw/**/*.scss', '*.html'], build)
 }
 
 exports.default = series(build, liveTask)
+
+//development
+//const { src, dest, watch, series } = require('gulp');
+//const sass = require('gulp-sass')(require('sass'));
+//
+//function build() {
+//    return src('ncw/**/*.scss')
+//        .pipe(sass().on('error', sass.logError))
+//        .pipe(dest('/css'))
+//}
+//
+//function liveTask() {
+//    watch(['ncw/**/*.scss'], build)
+//}
+
+//exports.default = series(build, liveTask)
