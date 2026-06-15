73739b8b1fc5d9cdca5f0c9f6d668883c30a12bf
cngumbi35 <cngumbi35@gmail.com>
2026-06-15 19:03:48 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
+
 function build() {
     return src('ncw/**/*.scss')
         .pipe(sass().on('error', sass.logError))
